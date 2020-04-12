4a1bf0e2d5c8fafe33cb3989b5764f3fb853053a
varshaahuja <va322@njit.edu>
2020-04-11 23:05:09 -0400

    updated login-authentication test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,15 +13,31 @@ def app():
 def test_index(client):
     assert client.get(url_for('main.index')).status_code == 200
 
+
 def test_getLogin(client):
     assert client.get(url_for('auth.login')).status_code == 200
+
 
 def test_getSignup(client):
     assert client.get(url_for('auth.signup')).status_code == 200
 
-
-
 def test_login(client):
-    rv = client.post(url_for('auth.login'),{'email':'saikiran1298gmail.com', 'password':'sachej-5fivxo-rIwbod'})
+    rv = client.post(url_for('auth.login'), data={'email':'va322@njit.edu', 'password':'va322@njit'})
     print(rv)
-    assert rv.status_code==302
+    assert rv.status_code == 302
+
+'''
+def test_login(client):
+    d = client.get(url_for('auth.login'))
+    response = client.post(d,
+                           data=dict(email='patkennedy79@gmail.com', password='FlaskIsAwesome', name='Pat'),
+                           follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Welcome, Pat!" in response.data
+    assert b"Open Calculator" in response.data
+    assert b"Home" in response.data
+    assert b"Profile" in response.data
+    assert b"Calculator" in response.data
+    assert b"Logout" in response.data
+    assert b"Login" not in response.data
+    assert b"Register" not in response.data'''
